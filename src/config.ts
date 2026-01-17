c32ee43070f9c281a187f0c515174bf283be3443
Daniel Mangasarian <danlucksome@gmail.com>
2026-01-16 21:52:01 -0800

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,25 @@
+export const siteConfig = {
+  title: "Menooa Avrand",
+  description:
+    "Vehicle Hardware and Dynamics Team Lead at CalSol. Studying Mech Engr at the University of California, Berkeley.",
+  author: "Menooa Avrand",
+  url: "https://www.menooaavrand.com",
+  themeColor: "#111010",
+} as const;
+
+export const personalConfig = {
+  name: "menooa",
+  emoji: "👋",
+  description: `
+  I currently work as a Vehicle Hardware and Dynamics Team Lead at [Cal Sol](https://calsol.berkeley.edu/). I previously worked at [Ferrari](https://www.ferrari.com/en-US) as a Mechanical Engineering Intern.
+  
+  I'm currently studying Mechanical Engineering at [University of California, Berkeley](https://me.berkeley.edu/).`,
+  externalLinks: [
+    { href: "https://www.linkedin.com/in/menooaavrand/", text: "linkedin" },
+    { href: "mailto:menooaavrand@berkeley.edu", text: "email" },
+    { href: "/Menooa_Avrand_resume.pdf", text: "resume" },
+    { href: "/Menooa Avrand Project Portfolio.pdf", text: "portfolio" },
+
+
+  ],
+} as const;
